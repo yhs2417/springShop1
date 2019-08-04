ca70fd3bdaf815050419361effcffde47493e317
yhs2417 <yhs3307@hanmail.net>
2019-08-04 12:09:54 +0900

    1.03..

--- shopping1/src/test/java/kr/hyunnn/shop1/PwTest.java
+++ shopping1/src/test/java/kr/hyunnn/shop1/PwTest.java
@@ -1,23 +1,7 @@
 package kr.hyunnn.shop1;
 
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
-import org.junit.Test;
 
 public class PwTest {
 
-	@Test
-	public void Pw()
-	{
-		StandardPBEStringEncryptor enc=new StandardPBEStringEncryptor();
-		
-		enc.setAlgorithm("");
-		enc.setPassword("");
-		
-		System.out.println(enc.encrypt(""));
-		System.out.println(enc.encrypt(""));
-		System.out.println(enc.encrypt(""));
-		System.out.println(enc.encrypt(""));
-		
-	}
-	
+
 }
